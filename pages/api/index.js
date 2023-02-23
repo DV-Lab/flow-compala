fb11dde4d6a5b0ac75bc0dad8eb1ae399dc6a25f
tranvinh146 <tranvinhlx135@gmail.com>
2023-02-23 23:53:06 +0700

    add network in config

--- pages/api/index.js
+++ pages/api/index.js
@@ -2,4 +2,5 @@ import * as fcl from "@onflow/fcl";
 
 fcl.config({
   "accessNode.api": "https://rest-mainnet.onflow.org",
+  "flow.network": "mainnet",
 });
--- pages/api/plays/index.js
+++ pages/api/plays/index.js
@@ -3,6 +3,7 @@ import * as fcl from "@onflow/fcl";
 
 fcl.config({
   "accessNode.api": "https://rest-mainnet.onflow.org",
+  "flow.network": "mainnet",
 });
 
 export default async function handler(req, res) {
